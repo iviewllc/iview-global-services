6da09da7072b105b0be6502f31eece42c8c2fa72
msamala539 <samala.madhusudhan@gmail.com>
2025-03-18 22:14:39 -0500

    email

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
-document.querySelector("form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("Your message has been sent!");
-});
+<script>
+    (function() {
+        emailjs.init("p113sa_AqtamQCI02"); // Correct User ID
+
+        // Event listener for form submission
+        document.getElementById('contact-form').addEventListener('submit', function(event) {
+            event.preventDefault();
+
+            // Sending form data using EmailJS
+            emailjs.sendForm("service_dnm3t6l", "template_6po1xu9", this)
+                .then(() => {
+                    alert("Message Sent Successfully!"); // Success message
+                }, (err) => {
+                    alert("Failed to Send Message: " + err); // Error message
+                });
+        });
+    })();
+</script>
